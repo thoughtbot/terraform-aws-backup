2dd34cbf3331c503acf71cc5ab013b174cb04135
Olamide <olamide@thoughtbot.com>
2023-12-14 03:29:23 +0100

    Remove unused dependency flag

--- backup-organization-policy/main.tf
+++ backup-organization-policy/main.tf
@@ -1,8 +1,6 @@
 resource "aws_organizations_delegated_administrator" "management" {
   account_id        = var.delegate_account_id
   service_principal = "backup.amazonaws.com"
-
-  depends_on = [aws_organizations_organization.org]
 }
 
 resource "aws_backup_global_settings" "cross_account_backup" {
